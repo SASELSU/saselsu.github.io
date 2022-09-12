265d54932526bce981e0c79618b7ddf83c0ca151
SASELSU <113201380+SASELSU@users.noreply.github.com>
2022-09-12 00:11:23 -0500

    Delete SASE-WEBSITE directory

--- SASE-WEBSITE/weather.js
+++
@@ -1,5 +0,0 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Baton+Rouge&APPID=8c6a6fe0adb497d44d2e1be1c4249979&units=imperial')
-.then(response => response.json())
-.then(data => 
-    document.getElementById('weather').appendChild(document.createTextNode("weather: " + data['weather'][0]['main'] + "\ntemp: " + data['main']['temp'] + "\nhumitiy: " + data['main']['humidity']))
-    )
