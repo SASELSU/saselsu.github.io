87503a28bf21cd4bf64d75c5ec0f2b75cae33116
Ricky Liang <liangricky7@gmail.com>
2024-08-30 22:48:37 -0500

    Really Barebones Event Posts Implemented

--- sase-website/src/pages/events.js
+++ sase-website/src/pages/events.js
@@ -1,7 +1,28 @@
 import React from "react";
 import "../styles/Events.css"
 
-import TitleCardBackground from "../assets/events/eventPaint.png"
+import TitleCardBackground from "../assets/events/eventVolleyball.png"
+import BobaSocial from "../assets/events/fall-2024/GBM1-Fall-2024.png"
+
+const Post = (props) => {
+    const {
+        image,
+        eventDate,
+        postTitle,
+        postDesc,
+    } = props;
+    return (
+        <div className="eventPost">
+            <img src={image}/>
+            <div className="eventPostInfo">
+                <h1>{postTitle}</h1>
+                <h1>{eventDate}</h1>
+                <p>{postDesc}</p>
+            </div>
+        </div>
+    );
+}
+
 const Events = () => {
     return (
         <>
@@ -11,6 +32,22 @@ const Events = () => {
         >
             <div className="eventColorOverlay"/>
             <h1>events</h1>
+        </div>
+        <div className="eventPostList">
+            <Post
+            image={BobaSocial}
+            eventDate="September 5th"
+            postTitle="Boba Social"
+            postDesc="🩵IT’S THAT TIME OF THE SEMESTER!!🩵Get ready for our first GBM of the fall semester: BOBA SOCIAL!🧋🧋🧋
+            Bring your friends and come and see what we have ready for y’all this semester! There will be fun activities and drinks to go along 😉"
+            />
+                        <Post
+            image={BobaSocial}
+            eventDate="September 5th"
+            postTitle="Boba Social"
+            postDesc="🩵IT’S THAT TIME OF THE SEMESTER!!🩵Get ready for our first GBM of the fall semester: BOBA SOCIAL!🧋🧋🧋
+            Bring your friends and come and see what we have ready for y’all this semester! There will be fun activities and drinks to go along 😉"
+            />
         </div>
         </>
     )
