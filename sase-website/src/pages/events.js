db01a09f30a35222aae533cdf96d4e396b69d877
Ricky Liang <liangricky7@gmail.com>
2024-08-31 00:59:27 -0500

    Added the Upcoming and Future Post Design System
    Added Event System where the nextmost event is enlarged while the rest are smaller tabs.

--- sase-website/src/pages/events.js
+++ sase-website/src/pages/events.js
@@ -2,38 +2,42 @@ import React from "react";
 import "../styles/events.css"
 
 import TitleCardBackground from "../assets/events/eventVolleyball.png"
-import BobaSocial from "../assets/events/fall-2024/GBM1-Fall-2024.png"
 
-const Post = (props) => {
-    const {
-        image,
-        eventDate,
-        postTitle,
-        postDesc,
-    } = props;
-    return (
-        <div className="eventPost">
-            <img src={image}/>
-            <div className="eventPostInfo">
-                <h1>{postTitle}</h1>
-                <h1>{eventDate}</h1>
-                <p>{postDesc}</p>
-            </div>
-        </div>
-    );
-}
+//event images
+import BobaSocial from "../assets/events/fall-2024/GBM1-Fall-2024.png"
+import MockInterview from "../assets/events/fall-2024/mock-interviews.png"
+
+//helpers
+import {
+    Post,
+    UpcomingPost,
+} from "./helpers/eventsHelpers"
 
 const Events = () => {
     return (
         <>
-        <div 
-            className="eventTitleCard"
+        <div className="eventTitleCard"
             style={{backgroundImage: `url(${TitleCardBackground})`}}
         >
             <div className="eventColorOverlay"/>
             <h1>events</h1>
         </div>
         <div className="eventPostList">
+            <h1>Upcoming Events</h1>
+            <UpcomingPost
+            image={MockInterview}
+            eventDate="September 4th"
+            postTitle="Mock Interview"
+            postDesc="ejifajoiejfoiasejfoisejfodsjfoijsdafi yuh yuh yuh"
+            />
+            <div className="postContainer">
+            <Post
+            image={BobaSocial}
+            eventDate="September 5th"
+            postTitle="Boba Social"
+            postDesc="🩵IT’S THAT TIME OF THE SEMESTER!!🩵Get ready for our first GBM of the fall semester: BOBA SOCIAL!🧋🧋🧋
+            Bring your friends and come and see what we have ready for y’all this semester! There will be fun activities and drinks to go along 😉"
+            />
             <Post
             image={BobaSocial}
             eventDate="September 5th"
@@ -48,6 +52,9 @@ const Events = () => {
             postDesc="🩵IT’S THAT TIME OF THE SEMESTER!!🩵Get ready for our first GBM of the fall semester: BOBA SOCIAL!🧋🧋🧋
             Bring your friends and come and see what we have ready for y’all this semester! There will be fun activities and drinks to go along 😉"
             />
+            
+            </div>
+
         </div>
         </>
     )
---
+++ sase-website/src/pages/helpers/eventsHelpers.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "../../styles/events.css"
+
+export const Post = (props) => {
+    const {
+        image,
+        eventDate,
+        postTitle,
+        postDesc,
+    } = props;
+    return (
+        <div className="eventPost">
+            <img src={image}/>
+            <div className="eventPostInfo">
+                <div className="eventPostHeader">
+                    <h1>{postTitle}</h1>
+                    <p>{eventDate}</p>
+                </div>
+                <p>{postDesc}</p>
+            </div>
+        </div>
+    );
+}
+
+export const UpcomingPost = (props) => {
+    const {
+        image,
+        eventDate,
+        postTitle,
+        postDesc,
+    } = props;
+    return (
+        <div className="eventUpPost">
+            <img src={image}/>
+            <div className="eventUpPostInfo">
+                <div className="eventUpPostHeader">
+                    <h1>{postTitle}</h1>
+                    <p>{eventDate}</p>
+                </div>
+
+                <p>{postDesc}</p>
+            </div>
+        </div>
+    );
+}
