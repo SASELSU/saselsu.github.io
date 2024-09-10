5b72765bc3402168d4be82f5121806df74813a19
Ricky Liang <liangricky7@gmail.com>
2024-09-09 20:03:31 -0500

    Added Exxon Events

--- src/pages/events.js
+++ src/pages/events.js
@@ -9,7 +9,8 @@ import BobaSocial from "../assets/events/fall-2024/GBM1-Fall-2024.png"
 // import MockInterview from "../assets/events/fall-2024/mock-interviews.png"
 import Pickleball from "../assets/events/fall-2024/24Pickleball.png"
 import BloodDrive from "../assets/events/fall-2024/24BloodDrive.png"
-
+import ExxonMock from "../assets/events/fall-2024/exxonMock.jpg"
+import ExxonPlant from "../assets/events/fall-2024/exxonPlant.png"
 //helpers
 import {
     Post,
@@ -31,14 +32,13 @@ const Events = () => {
 
             <div className="postContainer"> {/*something something more manual input*/}
                 <Post
-                    image={BobaSocial}
+                    image={ExxonMock}
                     eventMonth="SEPT 2024"
-                    eventDay="5"
-                    eventTime="6:30pm-8:00pm"
-                    eventLocation="PFT 1100"
-                    postTitle="GBM 1: Boba Social"
-                    postDesc="IT’S THAT TIME OF THE SEMESTER! Get ready for our first GBM of the fall semester: BOBA SOCIAL!🧋
-                Bring your friends and come and see what we have ready for y’all this semester! There will be fun activities and drinks to go along! 😉"
+                    eventDay="10"
+                    eventTime="6:00pm-7:30pm"
+                    eventLocation="PFT 1200"
+                    postTitle="Exxon Mock Interviews"
+                    postDesc="🗣️ Get some final practice with your professional skills before the Career Fair! Feel free to also bring your resumes for a fresh set of eyes 👀 📎 "
                 />
                 <Post
                     image={Pickleball}
@@ -50,13 +50,13 @@ const Events = () => {
                     postDesc="We are partnered with DOW to provide y'all a free night of Pickleball! Come to Stacks Pickleball (Stage 4) and join us for food, fun, and networking!"
                 />
                 <Post
-                    image={BloodDrive}
+                    image={ExxonPlant}
                     eventMonth="SEPT 2024"
-                    eventDay="26"
-                    eventTime="9:00am-4:00pm"
-                    eventLocation="LSU Union 310"
-                    postTitle="Blood Drive"
-                    postDesc="Come to the Student Union to donate your blood! Donors will be given a free FIGHT CANCER shirt! Sign up with the QR code."
+                    eventDay="20"
+                    eventTime="9:30am-12:00pm"
+                    eventLocation="4045 Scenic Hwy"
+                    postTitle="Exxon Plant Tour"
+                    postDesc="ENGINEERS ONLY. Come visit Exxon's plant! It is on a first come, first serve basis so sign up fast!"
                 />
                 {/* <Post //never remove (this is the future event)
                 image={FutureEvent}
@@ -68,6 +68,17 @@ const Events = () => {
                 postDesc="Our officers are currently formulating a new event! Either that or you're reading this page during the summer. Check back soon! "
                 /> */}
                 {/* uncomment above if theres no events */}
+            </div>
+            <div className="postContainer">
+            <Post
+                    image={BloodDrive}
+                    eventMonth="SEPT 2024"
+                    eventDay="26"
+                    eventTime="9:00am-4:00pm"
+                    eventLocation="LSU Union 310"
+                    postTitle="Blood Drive"
+                    postDesc="Come to the Student Union to donate your blood! Donors will be given a free FIGHT CANCER shirt! Sign up with the QR code."
+                />
             </div>
 
         </div>
