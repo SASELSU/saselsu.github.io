b1933f6e1e56666bc86b7f8e3122e555f9b37388
Ricky Liang <liangricky7@gmail.com>
2024-09-12 01:27:55 -0500

    Added Titlecard

--- src/pages/eboard.js
+++ src/pages/eboard.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "../styles/eboard.css"
 
+import Backdrop from "../assets/eboard/eboardBackdrop.png"
+
 const EBoard = () => {
     return (
-        <div className="eBoard">
-            <h1>This Page is Under Construction! Check back soon!  <br/> (The Webmaster is currently overhauling the website)</h1>
+        <>
+        <div 
+            className="eBoardTitleCard"
+            style={{backgroundImage: `url(${Backdrop})`}}
+        >
+            <div className="eBoardColorOverlay"/>
+            <h1>EXECUTIVE BOARD</h1>
         </div>
+        {/* <div className="eBoard">
+            <h1>This Page is Under Construction! Check back soon!  <br/> (The Webmaster is currently overhauling the website)</h1>
+        </div> */}
+        </>
+
     )
 }
 
