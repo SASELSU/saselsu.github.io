89aeae2c6779fd419fd08a9232f78796de47a554
Ricky Liang <liangricky7@gmail.com>
2024-12-15 16:56:07 -0600

    Eboard Page Update

--- src/pages/eboard.js
+++ src/pages/eboard.js
@@ -7,17 +7,28 @@ import {
  } from "./helpers/eboardHelpers";
 
 //eboard pics
-import PresidentPic from "../assets/about/eboard/faris_khattak.png"
-import VicePresidentPic from "../assets/about/eboard/grishma_shrestha.png"
-import SecretaryPic from "../assets/about/eboard/jennie_tran.png"
-import TreasurerPic from "../assets/about/eboard/galvin_wong.png"
-import PublicRelationsPic from "../assets/about/eboard/camila_cuadra.png"
-import HistorianPic from "../assets/about/eboard/kim_nguyen.png"
-import GraphicDesignPic from "../assets/about/eboard/kris_lam.png"
-import WebmasterPic from "../assets/about/eboard/ricky_liang.png"
-import FundraisingPic from "../assets/about/eboard/winson_liu.png"
-import OutreachPic from "../assets/about/eboard/himangini_chauhan.png"
-import ProDevPic from "../assets/about/eboard/thanh_kirsch.png"
+// import PresidentPic from "../assets/about/eboard/faris_khattak.png"
+// import VicePresidentPic from "../assets/about/eboard/grishma_shrestha.png"
+// import SecretaryPic from "../assets/about/eboard/jennie_tran.png"
+// import TreasurerPic from "../assets/about/eboard/galvin_wong.png"
+// import PublicRelationsPic from "../assets/about/eboard/camila_cuadra.png"
+// import HistorianPic from "../assets/about/eboard/kim_nguyen.png"
+// import GraphicDesignPic from "../assets/about/eboard/kris_lam.png"
+// import WebmasterPic from "../assets/about/eboard/ricky_liang.png"
+// import FundraisingPic from "../assets/about/eboard/winson_liu.png"
+// import OutreachPic from "../assets/about/eboard/himangini_chauhan.png"
+// import ProDevPic from "../assets/about/eboard/thanh_kirsch.png"
+import PresidentPic from "../assets/about/eboard/big_faris.png"
+import VicePresidentPic from "../assets/about/eboard/big_grishma.png"
+import SecretaryPic from "../assets/about/eboard/big_jennie.png"
+import TreasurerPic from "../assets/about/eboard/big_galvin.png"
+import PublicRelationsPic from "../assets/about/eboard/big_camila.png"
+import HistorianPic from "../assets/about/eboard/big_kim.png"
+import GraphicDesignPic from "../assets/about/eboard/big_kris.png"
+import WebmasterPic from "../assets/about/eboard/big_ricky.png"
+import FundraisingPic from "../assets/about/eboard/big_winson.png"
+import OutreachPic from "../assets/about/eboard/big_himangini.png"
+import ProDevPic from "../assets/about/eboard/big_thanh.png"
 
 
 const EBoard = () => {
@@ -62,7 +73,7 @@ const EBoard = () => {
                 blurb="I joined SASE because I wanted to better my resume and interview skills to land a job. But I ended up staying because of the community that SASE offers. There are lots of really cool people in this club and its so fun connecting and getting to know everyone. SASE offers a place not just to provide better opportunities for people like me, but also to give them a place where they feel comfortable and have fun while getting those really sick jobs."
             />            
             <DescriptionCard
-                title="Public Relations Chair"
+                title="Public     Relations Chair"
                 name="Camila Cuadra"
                 major="Piano, Junior, Class of 26"
                 image={PublicRelationsPic}
