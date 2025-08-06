707ffeaed33edf2418efc1464a2ab611f03aee73
dtaing11 <157168922+dtaing11@users.noreply.github.com>
2025-08-06 15:27:59 -0500

    adding more element to the sky

---
+++ src/hackathon/components/cloud/cloud1.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import cloudImg from "../../assets/cloud/Cloud.svg";
+import "./cloud.css";
+
+function Cloud1() {
+  const eventDate = new Date("2025-11-07");
+  const today = new Date();
+  const timeDiff = eventDate - today;
+  const finalCount = Math.max(0, Math.ceil(timeDiff / (1000 * 60 * 60 * 24)));
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (count < finalCount) {
+      const interval = setInterval(() => {
+        setCount((prev) => {
+          if (prev >= finalCount) {
+            clearInterval(interval);
+            return prev;
+          }
+          return prev + 1;
+        });
+      }, 25); // speed of counting (ms)
+      return () => clearInterval(interval);
+    }
+  }, [count, finalCount]);
+
+  return (
+    <div className="cloud-container">
+      <img src={cloudImg} alt="Cloud" className="cloud-img" />
+      <div className="cloud-text">
+        <div className="cloud-number">{count}</div>
+        <div className="cloud-label">days left</div>
+      </div>
+    </div>
+  );
+}
+
+export default Cloud1;
---
+++ src/hackathon/components/cloud/cloud2.jsx
@@ -0,0 +1,38 @@
+import "./Cloud2.css"; 
+import GDG from "../../assets/logo/GDSC.png"
+
+function Cloud2() {
+  return (
+    <div className="floating-cloud">
+      <svg
+        viewBox="0 0 260 140"
+        xmlns="http://www.w3.org/2000/svg"
+        width="100%"
+        height="auto"
+      >
+        <path
+          d="
+            M40,100
+            C20,120 0,110 10,80
+            C-5,60 20,40 40,50
+            C45,30 70,30 80,50
+            C90,35 115,35 120,55
+            C130,40 160,45 165,65
+            C185,50 210,70 195,90
+            C220,85 230,110 200,115
+            C190,125 160,120 150,110
+            C140,125 110,125 100,110
+            C85,125 60,125 55,110
+            C40,120 40,120 40,100
+            Z"
+          fill="#ffffff"
+          stroke="#cccccc"
+          strokeWidth="3"
+        />
+      </svg>
+      <img src={GDG} alt="GDG Logo" className="cloud2-logo" />
+    </div>
+  );
+}
+
+export default Cloud2;
