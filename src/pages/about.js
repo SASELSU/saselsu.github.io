c9ff72e95b26ce6e162c6218cf47c6a2e9d1d6d2
hschuler13 <hschul04@gmail.com>
2025-09-01 11:43:15 -0500

    started working on timeline for about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,11 +4,15 @@ import {
 } from "react";
 import "../styles/about.css"
 
-import { HistoryCard,EBoardCard } from "./helpers/aboutHelpers";
+import { HistoryCard, EBoardCard } from "./helpers/aboutHelpers";
 
 import TitleCardBackground from "../assets/about/title_card.jpg"
 
 import OurMission from "../assets/about/our-mission-temp.jpeg"
+
+//timeline arrows
+import NextArrow from "../assets/about/history/arrow_next.svg"
+//import BackArrow from "../assets/about/history/arrow_back.svg"
 
 //history pics
 import FirstMeeting from "../assets/about/history/firstSASE.jpeg"
@@ -36,67 +40,93 @@ import usePageTracking from "../components/Common/TrafficTracker";
 
 
 const About = () => {
-    usePageTracking("About Page");    
+    usePageTracking("About Page");
     // const scrollRef = useHorizontalScroll();
     // const [index, setIndex] = React.useState;
+
+    // VARIABLES
+const elH = document.querySelectorAll(".timeline li > div");
+
+// START
+window.addEventListener("load", init);
+
+function init() {
+ 
+}
+
+// SET EQUAL HEIGHTS
+
+
     return (
         <>
-        <div 
-            className="aboutTitleCard"
-            style={{backgroundImage: `url(${TitleCardBackground})`}}
-        >
-            <div className="aboutColorOverlay"/>
-            <h1>ABOUT US</h1>
-        </div>
-        <div className="aboutMission">
+            <div
+                className="aboutTitleCard"
+                style={{ backgroundImage: `url(${TitleCardBackground})` }}
+            >
+                <div className="aboutColorOverlay" />
+                <h1>ABOUT US</h1>
+            </div>
+            <div className="aboutMission">
                 <h1>Our Mission</h1>
                 <p>SASE is dedicated to the advancement of Asian heritage scientists and engineers in education and employment so that they can achieve their full career potential. In addition to professional development, SASE also encourages members to contribute to the enhancement of the communities in which they live.</p>
-                <img src={OurMission} alt="missionImage"/>
-                
+                <img src={OurMission} alt="missionImage" />
+
                 <p>SASE’s mission is to:
 
-Prepare Asian heritage scientists and engineers for success in the global business world.
-Celebrate intercultural exchanges on campuses and in the workplace.
-Provide opportunities for members to make contributions to their local communities.</p>
+                    Prepare Asian heritage scientists and engineers for success in the global business world.
+                    Celebrate intercultural exchanges on campuses and in the workplace.
+                    Provide opportunities for members to make contributions to their local communities.</p>
                 <p><b></b></p>
-        </div>
-        <div className="aboutHistory">
-            <h1>OUR HISTORY</h1>
-            <div className="historyCardSlider">
-                <div className="historyCardContainer"
-                // style={{transform: `translate3d(${-index * 100}%, 0, 0)`}}
-                > {/*ref={scrollRef} */}
-                    <HistoryCard 
-                    image={FirstMeeting}
-                    cardBlurb="SASE LSU emerged in February 2019, initially comprising a modest group of 25 active members."/>
-                    
-                    <HistoryCard
-                    image={ZoomCall}
-                    cardBlurb="However, the unforeseen impact of COVID-19 created a disconnect between the LSU community and its students. Alongside the challenges of virtual interactions and the concerning rise in AAPI violence nationwide, students within the LSU community felt increasingly isolated."/>
-                    
-                    <HistoryCard
-                    image={ReturnSASE}
-                    cardBlurb="Nevertheless, SASE LSU preserved, dedicating its efforts into engaging faculty, organizing interactive events, and highlighting accomplishments of current students and faculties. These endeavors aimed to generate awareness and initiate discussions centered around empowerment and community."/>
-                    
-                    <HistoryCard
-                    image={uhhhName}
-                    cardBlurb="As Fall 2021 approached, SASE LSU gained momentum by prioritizing the development of a tightly-knit community. With the gradual decline of restrictions, we successfully brought students together after a prolonged period of isolation."/>
-                    
-                    <HistoryCard
-                    image={FirstPaint}
-                    cardBlurb="We organized events such as boba and paint socials, volunteering events with organizations like Cat Haven and Habitat for Humanity, and giveback nights at local Asian businesses. These initiatives fostered a safe environment and bolstered our presence within the LSU and Baton Rouge community."/>
-                    
-                    <HistoryCard
-                    image={LunarMeeting}
-                    cardBlurb="Through persistent dedication throughout the semester, our membership skyrocketed from the initial 25 to a remarkable 133 active participants by the spring of 2022. We remained steadfast in our commitment to enhancing visibility and nurturing a sense of community. Our Lunar New Year meeting exemplified the celebration of intercultural exchange, acting as a testament to our growth. "/>
-                    
-                    <HistoryCard
-                    image={ReallyBigGathering}
-                    cardBlurb="As we expanded, we incorporated workshops and invited guest speakers to enable our members to pursue professional development. SASE LSU serves as a space where individuals can embrace intercultural exchange against all odds, forge their own community within LSU, and, above all, to love purple and live gold."/>
-                </div>    
             </div>
-        
-        </div>
+            
+            <section class="timeline">
+                    <ol>
+                    <li>
+                        <div>
+                            <img src={FirstMeeting}></img>
+                            <p>SASE LSU emerged in February 2019, initially comprising a modest group of 25 active members.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <p>However, the unforeseen impact of COVID-19 created a disconnect between the LSU community and its students. Alongside the challenges of virtual interactions and the concerning rise in AAPI violence nationwide, students within the LSU community felt increasingly isolated</p>
+                            <img src={ZoomCall}></img>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <p>Nevertheless, SASE LSU preserved, dedicating its efforts into engaging faculty, organizing interactive events, and highlighting accomplishments of current students and faculties. These endeavors aimed to generate awareness and initiate discussions centered around empowerment and community.</p>
+                            <img src={ReturnSASE}></img>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <p>As Fall 2021 approached, SASE LSU gained momentum by prioritizing the development of a tightly-knit community. With the gradual decline of restrictions, we successfully brought students together after a prolonged period of isolation.</p>
+                            <img src={uhhhName}></img>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <p>We organized events such as boba and paint socials, volunteering events with organizations like Cat Haven and Habitat for Humanity, and giveback nights at local Asian businesses. These initiatives fostered a safe environment and bolstered our presence within the LSU and Baton Rouge community.</p>
+                            <img src={FirstPaint}></img>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <p>With persistent dedication throughout the semester, membership skyrocketed to a remarkable 133 active participants by the spring of 2022. We remained steadfast in our commitment to enhancing visibility and nurturing a sense of community. Our Lunar New Year meeting exemplified the celebration of intercultural exchange, acting as a testament to our growth.</p>
+                            <img src={LunarMeeting}></img>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <p>As we expanded, we incorporated workshops and invited guest speakers to enable our members to pursue professional development. SASE LSU serves as a space where individuals can embrace intercultural exchange against all odds, forge their own community within LSU, and, above all, to love purple and live gold.</p>
+                            <img src={ReallyBigGathering}></img>
+                        </div>
+                    </li>
+                </ol>
+            </section>
+
+
         </>
     )
 }
