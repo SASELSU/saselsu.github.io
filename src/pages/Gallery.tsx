5409884bdcbe7ca0966dc10ebf09f7736d323360
hschuler13 <hschul04@gmail.com>
2026-01-15 08:40:59 -0600

    additional css changes

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,11 +1,11 @@
+// TODO: go all the way from 2019 and add events to the google calendar with the proper format
+// (im not getting any help with this am i?)
+
 import React, { useEffect, useState } from "react";
 import "../styles/gallery.css";
 
 import Backdrop from "../assets/gallery/galleryBackdrop.png";
 import { GalleryCard } from "./helpers/galleryHelpers";
-
-/* fall 24 */
-import Quinlan from "../assets/gallery/fall24/quinlan.jpg";
 
 import usePageTracking from "../components/Common/TrafficTracker";
 
@@ -79,10 +79,10 @@ const Gallery = () => {
   usePageTracking("Gallery Page");
 
   const [gallery, setGallery] = useState([]);
-  const [semester, setSemester] = useState("fall");
+  const [semester, setSemester] = useState("spring");
   const [semesterStart, setSemesterStart] = useState("");
   const [semesterEnd, setSemesterEnd] = useState("");
-  const [year, setYear] = useState("2025");
+  const [year, setYear] = useState("2026");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -149,70 +149,6 @@ const Gallery = () => {
     console.log(galleryList);
   };
 
-  // display information fetched from the Google Calendar
-  // activates when page first loads, if there are changes, user must reload the page
-  /*useEffect(() => {
-    async function fetchUpcomingEvents() {
-      const timeMin = new Date().toISOString();
-
-      const url = `https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent(
-        calendarId
-      )}/events?key=${apiKey}&singleEvents=true&orderBy=startTime&timeMin=${timeMin}&maxResults=5`;
-
-      const res = await fetch(url);
-      const data = await res.json();
-
-      const eventList = data.items.map(
-        (event: { summary: string; location: string; description: string }) => {
-          const title = event.summary || "No title";
-          const { date, time } = formatEventDate(event);
-          const location = event.location || "No location provided";
-
-          let desc = "No description provided";
-          let imgUrl = "";
-          let galleryUrl = "#";
-
-          // uncomment when vincent updates the facebook page (take your time bro)
-          if (event.description) {
-            const parts = event.description.split(",");
-            desc = parts[0]?.trim() || "No description provided";
-
-            let imgCandidate = parts[1]?.trim() || "";
-            const match = imgCandidate.match(/href="([^"]+)"/);
-            if (match?.[1]) {
-              imgCandidate = match[1];
-            }
-            imgUrl = imgCandidate;
-
-            let galleryLink = parts[2]?.trim() || "";
-            const matchGallery = galleryLink.match(/href="([^"]+)"/);
-            if (matchGallery?.[1]) {
-              galleryLink = matchGallery[1];
-            }
-            galleryUrl = galleryLink;
-          }
-
-          return {
-            title,
-            date,
-            time,
-            location,
-            description: desc,
-            image: imgUrl,
-            gallery: galleryUrl,
-          };
-        }
-      );
-
-      setEvents(eventList);
-
-      // console debug
-      console.log("Fetched events:", eventList);
-    }
-
-    void fetchUpcomingEvents();
-  }, []);*/
-
   return (
     <>
       <div
@@ -224,18 +160,22 @@ const Gallery = () => {
       </div>
 
       <div className="gallery">
-        <div>
+        <div style={{ width: "30%", margin: "auto", marginTop: "2.5%", marginBottom: "2.5%", display: "flex", justifyContent: "center", alignItems: "center", padding: "1%", background: "white", borderRadius: "20px"}}>
           <form onSubmit={handleSubmit}>
             {/* Semester dropdown */}
+            <label>Semester: </label>
             <select
               value={semester}
               onChange={(e) => setSemester(e.target.value)}
+              style={{marginRight: "2%"}}
             >
               <option value="fall">fall</option>
               <option value="spring">spring</option>
             </select>
-
+            
             {/* Year dropdown */}
+            {/* TODO: Make this automatically add a year every year*/}
+            <label>Year: </label>
             <select value={year} onChange={(e) => setYear(e.target.value)}>
               <option value="2019">2019</option>
               <option value="2020">2020</option>
@@ -244,19 +184,13 @@ const Gallery = () => {
               <option value="2023">2023</option>
               <option value="2024">2024</option>
               <option value="2025">2025</option>
+              <option value="2026">2026</option>
             </select>
 
-            <button type="submit">Submit</button>
+            <button type="submit" style={{ border: "none", borderRadius: "5px", color: "white", background: "rgb(0, 78, 100)", fontFamily: ""}}>Submit</button>
           </form>
         </div>
         <div className="galleryCardContainer">
-          {/*<GalleryCard
-              key={1}
-              image={'https://scontent-dfw5-2.cdninstagram.com/v/t51.75761-15/488368746_18136706764394884_3088724195772795059_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=106&ig_cache_key=MzYxMDcxODYzMjk3MzYyMzgzNA%3D%3D.3-ccb1-7&ccb=1-7&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjEwODB4MTA4MC5zZHIuQzMifQ%3D%3D&_nc_ohc=GJd2jsrP2GUQ7kNvwFW-bPY&_nc_oc=AdklJjvN89w7JyBJbxnbzRFffyId8s_dpYJwIC6EuHVwgprmfRYjW2BDcE_BaxNef9V1UglD77s_Uye5UcrRUNI8&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-dfw5-2.cdninstagram.com&_nc_gid=Wt2VgoRQAPGjXhJLLIPLZg&oh=00_AfYuYYDT4A_riuE79Q89VRaO5b6XZuhXN2qHbvgdbVqpig&oe=68E46512'}
-              title={'Relax-SASE-tion'}
-              date={'April 22nd'}
-              link={'https://www.facebook.com/media/set/?set=a.1116871087135206&type=3'}
-            />*/}
           {gallery.map((gal, i) => (
             <GalleryCard
               key={i}
